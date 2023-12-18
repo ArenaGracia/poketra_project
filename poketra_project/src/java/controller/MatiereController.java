d8331c86361591057e09975ff8f7ba6394107626
tsoa-iangotiana <152261956+tsoa-iangotiana@users.noreply.github.com>
2023-12-18 21:33:00 +0300

    ajoutMatiereLook

--- poketra_project/src/java/controller/MatiereController.java
+++ poketra_project/src/java/controller/MatiereController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modele.Matiere;
-import modele.Unite;
+import utils.Unite;
 
 /**
  *
