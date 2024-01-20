80aac4ba3827e47e53c39f74636552fd8682a0d0
tsoa-iangotiana <152261956+tsoa-iangotiana@users.noreply.github.com>
2024-01-20 23:11:28 +0300

    ajoutSpecialitePrix avec Controller

---
+++ poketra_project/src/java/controller/specialite/SpecialitePrixController.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.specialite;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modele.Specialite;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SpecialitePrixController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try{
+            Specialite spec = new Specialite();
+            ArrayList<Specialite> liste=spec.getAll(null);
+            request.setAttribute("specialites", liste);
+        }catch(Exception e){
+            request.setAttribute("erreur", e.getMessage());
+        }finally{
+            request.getRequestDispatcher("./pages/admin/ajoutPrixSpecialite.jsp").forward(request, response);
+        }    
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      try{
+        double price = Double.valueOf(request.getParameter("salaire"));
+        String idSpec= request.getParameter("specialite");
+       Specialite spec=new Specialite();
+       spec.setId(idSpec);
+        spec.setSalaire(price);
+        spec.inserer(null);
+        request.setAttribute("message","insertion reussi");
+     }  catch(Exception e){
+         request.setAttribute("erreur",e.getMessage());
+     }
+     finally{
+         processRequest(request,response);
+     }
+   }
+}
---
+++ poketra_project/web/pages/admin/ajoutPrixSpecialite.jsp
@@ -0,0 +1,50 @@
+<%-- 
+    Document   : ajoutPrixSpecialite
+    Created on : 20 janv. 2024, 22:32:05
+    Author     : ASUS
+--%>
+
+<%@page import="modele.Specialite"%>
+<%@page import="java.util.ArrayList"%>
+<%
+    String message=(String) request.getAttribute("message");
+    String erreur=(String) request.getAttribute("erreur");
+    ArrayList<Specialite> listeU=(ArrayList<Specialite>) request.getAttribute("specialites");
+ %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:include page="headerAdmin.jsp" />
+    <div class="col-md-6 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Inserer un prix pour une specialite</h4>
+        <form class="forms-sample" action="<%= request.getContextPath() %>/add_specialite_prix" method="post">
+            <div class="input-group mb-2 mr-sm-2">
+                <select class="form-control" name="specialite">
+                <% if(listeU!=null) { %>
+                    <% for(int i=0;i<listeU.size();i++) { %>
+                        <option value="<%= listeU.get(i).getId()%>"><%= listeU.get(i).getNom() %></option>
+                    <% } %>
+                <% } %>
+                </select>
+            </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Salaire</label>
+            <input type="text" class="form-control" name="salaire">
+          </div>             
+          <%if(message != null){ %>
+            <div class="alert alert-success" role="alert">
+                <p><%= message %></p>
+            </div>
+          <%   } %>
+          <%if(erreur != null){ %>
+            <div class="alert alert-danger" role="alert">
+                <p><%= erreur %></p>
+            </div>
+          <%   } %>
+          <input type="submit" value="Valider" class="btn btn-primary mr-2" />
+        </form>
+      </div>
+    </div>
+  </div>    
+<jsp:include page="footerAdmin.jsp" />
+
